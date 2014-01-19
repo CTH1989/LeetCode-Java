792d2c6e4e687732f2206d7f2ad066d405d78559
CTH1989 <tianhao.c@gmail.com>
2014-01-19 05:05:49 -0500

    3Sum (without hash when checking eligibility)

--- 3Sum/Solution.java
+++ 3Sum/Solution.java
@@ -1,48 +1,36 @@
 public class Solution {
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        HashMap<ArrayList<Integer>, Boolean> mapA = new HashMap<ArrayList<Integer>, Boolean>(); 
+        HashMap<ArrayList<Integer>, Boolean> map = new HashMap<ArrayList<Integer>, Boolean>(); 
         int len = num.length;
-        int ele[] = new int[3];
-
-        for (int Ele : num) {
-            if (map.containsKey(Ele)) {
-                int tmp = map.get(Ele);
-                map.put(Ele, tmp + 1);
-            } else
-                map.put(Ele, 1);
-        }
-
-        for (int i = 0; i < len - 2; i++) {
-            ele[0] = map.get(num[i]);
-            if (ele[0] != 0) {
-                map.put(num[i], ele[0] - 1);
-                for (int j = i + 1; j < len; j++) {
-                    ele[1] = map.get(num[j]);
-                    if (ele[1] != 0) {
-                        int target = (-1) * (num[i] + num[j]);
-                        if (map.containsKey(target)) {
-                            ele[2] = map.get(target);
-                            if ((num[j] != target && ele[2] != 0) || (num[j] == target && ele[2] > 1)) {
-                                ArrayList<Integer> ret = new ArrayList<Integer>();
-                                ret.add(num[i]);
-                                ret.add(num[j]);
-                                ret.add(target);
-                                Collections.sort(ret);
-                                if (!mapA.containsKey(ret)) {
-                                    ans.add(ret);
-                                    mapA.put(ret, true);
-                                }
-                            }
-                        }
-                        
+        
+        Arrays.sort(num);
+        
+        for (int i = 0; i < len; i ++) {
+            int j = i + 1;
+            int k = len - 1;
+            while (j < k) {
+                int tmp = num[i] + num[j] + num[k];
+                if (tmp == 0) {
+                    ArrayList<Integer> ret = new ArrayList<Integer>();
+                    ret.add(num[i]);
+                    ret.add(num[j]);
+                    ret.add(num[k]);
+                    Collections.sort(ret);
+                    if (!map.containsKey(ret)) {
+                        ans.add(ret);
+                        map.put(ret, true);
                     }
+                    j ++;
+                    k --;
+                } else if (tmp > 0) {
+                    k --;
+                } else {
+                    j ++;
                 }
             }
-            map.put(num[i], 0);
         }
-
+        
         return ans;
     }
     /*
@@ -50,5 +38,6 @@ public class Solution {
     {-2,0,0,2,2};
     {0, 0, 0};
     {1 ,0, -1, 1, 2, -1};
+    {-2,0,1,1,2};
     */
 }
