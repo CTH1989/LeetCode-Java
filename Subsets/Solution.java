c571c388577856f92115eb10019f2b51b12c35a0
CTH1989 <tianhao.c@gmail.com>
2014-03-16 08:30:57 -0400

    Subsets (without recursive)

--- Subsets/Solution.java
+++ Subsets/Solution.java
@@ -1,28 +1,19 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> powerset(int[] S, int n) {
-        if (n == -1) {
-            ArrayList<Integer> ret = new ArrayList<Integer>();
-            ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-            ans.add(ret);
-            return ans;
-        }
-        ArrayList<ArrayList<Integer>> ret = powerset(S, n - 1);
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-        int len = ret.size();
-        ans.addAll(ret);
-        for (int i = 0; i < len; i++) {
-            ArrayList<Integer> np = (ArrayList<Integer>) ret.get(i).clone();
-            np.add(S[n]);
-            ans.add(np);
-        }
-        return ans;
-    }
     public ArrayList<ArrayList<Integer>> subsets(int[] S) {
         int len = S.length;
         if (len == 0)
             return new ArrayList<ArrayList<Integer>>();
         Arrays.sort(S);
-        ArrayList<ArrayList<Integer>> ans = powerset(S, len - 1);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        ans.add(new ArrayList<Integer>());
+        for (int i = 0; i < len; i++) {
+            int num = ans.size();
+            for (int j = 0; j < num; j++) {
+                ArrayList<Integer> np = (ArrayList<Integer>) ans.get(j).clone();
+                np.add(S[i]);
+                ans.add(np);
+            }
+        }
         return ans;
     }
 }
