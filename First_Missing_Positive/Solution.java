61242863116cd10e2d4344f1296f78f2b7a209ee
CTH1989 <tianhao.c@gmail.com>
2014-04-23 02:14:17 -0400

    First Missing Positive (constant space)

--- First_Missing_Positive/Solution.java
+++ First_Missing_Positive/Solution.java
@@ -1,13 +1,21 @@
 public class Solution {
     public int firstMissingPositive(int[] A) {
-        int len = A.length, i;
-        HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-        for (i = 0; i < len; i ++) {
-            if (A[i] > 0) map.put(A[i], true);
+        int n = A.length;
+        if (n == 0)
+            return 1;
+        int i = 0;
+        while (i < n) {
+            if (A[i] != (i + 1) && A[i] >= 1 && A[i] <= n && A[i] != A[A[i] - 1]) {
+                int tmp = A[A[i] - 1];
+                A[A[i] - 1] = A[i];
+                A[i] = tmp;
+            } else
+                i++;
         }
-        for (i = 1; i <= len; i ++) {
-            if (!map.containsKey(i)) break;
+        for (i = 0; i < n; i++) {
+            if (A[i] != (i + 1))
+                return i + 1;
         }
-        return i;
+        return n + 1;
     }
 }
